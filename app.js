728a4de2a2579dce1319b27be63eb2dc7b4afc28
LaneHoeppner <lhoeppner2@ivytech.edu>
2024-12-04 05:51:06 -0500

    video 9.1

--- app.js
+++ app.js
@@ -1,21 +1,66 @@
 const express= require('express');
-const { method } = require('lodash');
+const { method, result } = require('lodash');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 //express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://lhoeppner2:test@cluster0.z63lj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(dbURI)
+.then ((result) => app.listen(3000))
+.catch((err) => console.log(err));
+
+
 //register view engine
 app.set('view engine', 'ejs');
 
-//listen
-app.listen(3000); 
+ 
 
 //middleware and static files
 
 app.use(express.static('public'));
 
 app.use(morgan('dev'));
+
+//mongoose and mongo routes
+app.get('/add-blog', (req,res) => {
+    const blog = new Blog({
+        title:'new blog2',
+        snippet: 'asdfasdf',
+        body: 'morewasdf'
+    });
+
+    blog.save()
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/all-blogs', (req,res) => {
+    Blog.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.get('/single-blog', (req,res) => {
+    Blog.findById('67502eaf44a84cc3c057c695')
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 
 app.get('/', (req,res) =>{
