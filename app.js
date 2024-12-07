530c3941c89e13ae6330ab0290a8a1e445abb7a5
Lane <lhoeppner2@ivytech.edu>
2024-12-07 10:51:13 -0500

    video 11

--- app.js
+++ app.js
@@ -1,45 +1,43 @@
 const express= require('express');
-const { method } = require('lodash');
 const morgan = require('morgan');
-const dbURI = 'mongodb://tropicthunder:<tropicthunder>@<hostname>/?ssl=true&replicaSet=atlas-11qgdv-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
 
 //express app
 const app = express();
 
-//register view engine
+const dbURI = 'mongodb+srv://lane:pass@cluster0.icrst.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(dbURI)
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+// register view engine
 app.set('view engine', 'ejs');
 
-//listen
-app.listen(3000); 
-
-//middleware and static files
-
+// middleware & static files
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
-
-app.get('/', (req,res) =>{
-    //res.send('<p>home page</p>');
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index', {title: 'Home', blogs});
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
 });
 
-app.get('/about', (req,res) =>{
-   // res.send('<p>about page</p>');
-   res.render('about', {title: 'About'});
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
 });
 
-app.get('/blogs/create', (req,res) => {
-    res.render('create', {title: 'Create a new blog'});
-})
-
-//404
-app.use((req,res) => {
-    res.status(404).render('404', {title: '404'});
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
